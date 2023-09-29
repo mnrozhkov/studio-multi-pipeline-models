c284e6b28a49fdc7e18a0e4fe980d8af0b1d5756
Mikhail <mnrozhkov@gmail.com>
2023-09-29 17:40:47 +0200

    Update run.sh - use template_dvc.yaml

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ echo "Project/Model: $1"
 echo "Division/Data: $2"
 
 echo "Copy a $1:dvc.yaml template to pipeline: pipelines/$1/$2"
-cp pipelines/$1/template/dvc.yaml pipelines/$1/$2
+cp pipelines/$1/template_dvc.yaml pipelines/$1/$2/dvc.yaml
 
 echo "Run DVC pipeline: pipelines/$1/$2"
-dvc exp run -R pipelines/$1/$2
+dvc exp run -R pipelines/$1/$2/dvc.yaml
